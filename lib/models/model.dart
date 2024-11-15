76279e704bb72d1175af55bcc9aeb28518144f54
Kassem Hmady <khmkhm626@gmail.com>
2024-11-16 01:43:08 +0200

    Created files to represent the data we will be fetching from the API requests.

---
+++ lib/models/model.dart
@@ -0,0 +1,79 @@
+library model;
+
+List<String> idList = [
+"4328",
+"4329",
+"4330",
+"4331",
+"4332",
+"4334",
+"4335",
+"4336",
+"4337",
+"4338",
+"4339",
+"4340",
+"4344",
+"4350",
+"4354",
+"4355",
+"4356",
+"4380",
+"4381",
+"4387",
+"4388",
+"4394",
+"4395",
+"4396",
+"4397",
+"4398",
+"4399",
+"4400",
+"4401",
+"4408",
+];
+
+List<String> bannerUrlList = [
+"https://www.thesportsdb.com/images/media/league/badge/dsnjpz1679951317.png",
+"https://www.thesportsdb.com/images/media/league/badge/ty5a681688770169.png",
+"https://www.thesportsdb.com/images/media/league/badge/72d3zc1688333496.png",
+"https://www.thesportsdb.com/images/media/league/badge/teqh1b1679952008.png",
+"https://www.thesportsdb.com/images/media/league/badge/67q3q21679951383.png",
+"https://www.thesportsdb.com/images/media/league/badge/zcafvy1719637069.png",
+"https://www.thesportsdb.com/images/media/league/badge/ja4it51687628717.png",
+"https://www.thesportsdb.com/images/media/league/badge/jm7ky01602788843.png",
+"https://www.thesportsdb.com/images/media/league/badge/1hbggv1722770851.png",
+"https://www.thesportsdb.com/images/media/league/badge/mjit7n1593634474.png",
+"https://www.thesportsdb.com/images/media/league/badge/h7xx231601671132.png",
+"https://www.thesportsdb.com/images/media/league/badge/28uq381687624585.png",
+"https://www.thesportsdb.com/images/media/league/badge/rp1i7e1689641585.png",
+"https://www.thesportsdb.com/images/media/league/badge/mav5rx1686157960.png",
+"https://www.thesportsdb.com/images/media/league/badge/qprvpy1471773025.png",
+"https://www.thesportsdb.com/images/media/league/badge/d4yp7g1690178551.png",
+"https://www.thesportsdb.com/images/media/league/badge/2u78lm1638459575.png",
+"https://www.thesportsdb.com/images/media/league/badge/4cem2k1619616539.png",
+"https://www.thesportsdb.com/images/media/league/badge/klfgou1649620913.png",
+"https://www.thesportsdb.com/images/media/league/badge/frdjqy1536585083.png",
+"https://www.thesportsdb.com/images/media/league/badge/u96k271549480775.png",
+"https://www.thesportsdb.com/images/media/league/badge/uf5kph1598011132.png",
+"https://www.thesportsdb.com/images/media/league/badge/i57nok1721301867.png",
+"https://www.thesportsdb.com/images/media/league/badge/afedb31688770443.png",
+"https://www.thesportsdb.com/images/media/league/badge/jmb3ms1688770451.png",
+"https://www.thesportsdb.com/images/media/league/badge/3gzcx81720176510.png",
+"https://www.thesportsdb.com/images/media/league/badge/hl40981534764789.png",
+"https://www.thesportsdb.com/images/media/league/badge/r7u6821688425700.png",
+"https://www.thesportsdb.com/images/media/league/badge/j7nbg91719637466.png",
+"https://www.thesportsdb.com/images/media/league/badge/4n3h6z1572778356.png",
+];
+
+class League {
+  int id = -1;
+  String name = "";
+  String sport = "";
+  String bannerUrl = "";
+}
+
+class Event {
+  String titl = "";
+  String timeStamp = "";
+}
---
+++ lib/utils/http_getters.dart
@@ -0,0 +1,9 @@
+library http_getters;
+
+import '../models/model.dart';
+import 'package:http/http.dart' as http;
+
+List<League> getLeagues()
+{
+  return List<League>.empty();
+}
